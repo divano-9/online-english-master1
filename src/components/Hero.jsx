2c48e111f0ef72b90bc2c3ab49caea08ff138937
divano-9 <dusanivanovic@mail.com>
2024-03-20 01:20:45 +0100

    video section done
    -added video section/component
    -edited background svgs

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,12 @@
 import Navigation from "../components/Navigation";
-import bot from "../assets/hero-background.svg";
+import bot from "../assets/hero-background2.svg";
 import bg from "../assets/hero-bg.svg";
 import mob from "../assets/hero-bg-mob.svg";
 import useWindowDimensions from "../utils/useWindowDimensions";
 
 const Hero = () => {
   const { height, width } = useWindowDimensions();
-  const breakpoint = 992;
+  const breakpoint = 700;
   return (
     <section className="hero">
       <Navigation />
@@ -17,7 +17,6 @@ const Hero = () => {
           <img src={bg} className="bg-img" />
         )}
       </div>
-
       <img src={bot} className="bot-img" />
     </section>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Hero from "../components/Hero";
+import Video from "../components/Video";
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <Video />
     </>
   );
 };
