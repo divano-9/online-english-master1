7463b5b66a4017458894475b0798ee6520223110
divano-9 <dusanivanovic@mail.com>
2024-06-21 15:51:53 +0200

    edited viet hero illustration text
    - edited vietnamese hero illustration (txt)
    -edited promo vietnamese text

--- src/components/mini-components/PromoViet.jsx
+++ src/components/mini-components/PromoViet.jsx
@@ -2,28 +2,12 @@ const PromoViet = () => {
   return (
     <div className="promo-txt viet">
       <p>
-        Đăng ký lớp học tiếng Anh trực tuyến Master 1 - Cánh cửa mở đến với Sự
-        tự tin và Cơ hội!
-      </p>
-
-      <p>
-        <strong>BẠN KHÔNG CẦN PHẢI TRẢ TIỀN NGAY B Y GIỜ!</strong>
-      </p>
-      <p>
-        <strong>MIỄN PHÍ BUỔI HỌC ĐẦU TIÊN.</strong>
-      </p>
-      <p>
-        Master 1 cung cấp các lớp học tiếng Anh trực tuyến ngay tại nhà bạn vào
-        thời gian phù hợp với bạn. Bạn là người chọn những gì bạn muốn học, khi
-        nào bạn muốn học và các lớp học là một-thầy-một-trò! Điều này có nghĩa
-        là bạn sẽ tiết kiệm thời gian và năng lượng, đồng thời tránh sự nhàm
-        chán khi phải chờ đợi đến lượt của mình như trong các lớp học nhóm! Hơn
-        nữa, chúng tôi giảng dạy cho cả trẻ em và người lớn.
-      </p>
-      <p>
-        Lên lịch BUỔI HỌC THỬ MIỄN PHÍ của bạn, làm quen với phương pháp giảng
-        dạy của chúng tôi và trở thành người nói tiếng Anh tự tin với sự giúp đỡ
-        của các người thầy bản địa Tiếng Anh với chuyên môn cao.
+        <strong>
+          Trải nghiệm các lớp học tiếng Anh trực tuyến cá nhân một-kèm-một với
+          giáo viên nước ngoài ngay tại sự thoải mái của ngôi nhà bạn với lịch
+          trình linh hoạt và buổi học đầu tiên miễn phí để nâng cao sự tự tin và
+          kỹ năng của bạn.
+        </strong>
       </p>
     </div>
   );
